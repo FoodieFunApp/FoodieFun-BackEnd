54baa2129bdf95b99535f1c540864fb45dc043bd
Ken Ruf <ruf.kenichi@gmail.com>
2019-09-04 22:49:47 -0500

    delete user

--- users/user-model.js
+++ users/user-model.js
@@ -1,9 +1,14 @@
 const db = require('../database/db-config.js');
 
 module.exports = {
-    addUser
+    addUser,
+    deleteUser
 }
 
-function addUser(user) {
-    db('users').insert(user)
+async function addUser(user) {
+    return await db('users').insert(user)
+}
+
+async function deleteUser(id) {
+    return await db('users').where({id}).del()
 }
