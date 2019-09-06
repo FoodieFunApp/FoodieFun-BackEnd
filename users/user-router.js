730a2c05c316591093999fa690e057aa22635e1f
Ken Ruf <ruf.kenichi@gmail.com>
2019-09-05 20:27:46 -0500

    validateUserId middleware

--- users/user-router.js
+++ users/user-router.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-const Users = require('./user-model.js')
+const Users = require('./user-model.js');
+
+router.get('/:userId', (req, res) => {
+
+})
+
+//Middlewares
+
+const validateUserId = async (req, res, next) => {
+    const {id} = req.params;
+    try {
+        const user = await Users.getUserBy({id});
+        if(user) {
+            req.user = user;
+            next();
+        } else {
+            res.status(400).json({message: `User with Id: ${id} does not exist`})
+        }
+    }
+    catch(error) {
+        res.status(500).json({message: "validateUserId Error", error: error})
+    }
+}
 
 module.exports = router;
