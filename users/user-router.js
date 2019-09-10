e4deea09b6838eaf5aebafe29e7709e5b85e152b
unknown <dj.zaragoza@icloud.com>
2019-09-09 20:45:59 -0700

    deleted middleware folder and added the user credentials validation and authorization middlewares in user-router file

--- users/user-router.js
+++ users/user-router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('./user-model.js');
 
@@ -27,20 +29,40 @@ async function validateUserId(req, res, next) {
     }
 }
 
-async function validateUserCredentials(req, res, next) {
-    const { userCredentials } = req.body;
-    try {
-        const user = await Users.getUserBy({ userCredentials });
-        if (user) {
-            req.user = user;
-            next();
-        } else {
-            res.status(400).json({ message: "user credentials invalid" })
-        }
-    }
-    catch (error) {
-        res.status(500).json({ message: "validateUserCredentials error", error: error })
-    }
+const { username, password } = req.body;
+
+if (username && password ) {
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                next();
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Ran into an unexpected error' });
+        });
+} else {
+    res.status(400).json({ message: 'No credentials provided' });
 }
+
+module.exports = (req, res, next) => {
+    const authorization = req.body.authorization;
+
+    if(authorization) {
+        jwt.verify(authorization, 'areyouforreal', (err, decodedToken) => {
+            if(err) {
+                res.status(401).json({message: 'areyouserious?'});
+            } else { 
+                req.decodedJwt = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({message: 'nope not gonna work'});
+    }
+};
 
 module.exports = router;
