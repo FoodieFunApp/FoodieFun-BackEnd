d1aa37d76453a154c675266b45692fd6acda07ee
unknown <dj.zaragoza@icloud.com>
2019-09-11 20:53:50 -0700

    adding endpoints GET and POST

--- users/user-router.js
+++ users/user-router.js
@@ -7,8 +7,16 @@ const Users = require('./user-model.js');
 
 //Routers
 
-router.get('/:userId', validateUserId, (req, res) => {
-    res.status(200).json(req.user)
+router.get('/:userId', validateUserId, async (req, res) => {
+    const {userId} = req.params;
+    const user = req.body;
+    try {
+        await Users(userId, user);
+        res.status(201).json({message: "User validated"});
+    }
+    catch(error) {
+        res.status(500).json({ message: "Could not validate user", error: error});
+    }
 })
 
 router.put('/:userId', validateUserId, async (req, res) => {
