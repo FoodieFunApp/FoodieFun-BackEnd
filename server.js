95183c8de266749e79d79e671465302b35322751
Eric Hendrix <agentmrbig@gmail.com>
2019-09-17 19:08:58 -0400

    fixed something I messed up in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const userRouter = require('./users/user-router.js')
 const authRouter = require('./auth/auth-router');
-const reviewRouter = require('./reviews/review-router');
+
 
 const server = express();
 
@@ -15,7 +15,7 @@ server.use(cors());
 
 server.use('/api/users', userRouter);
 server.use('/api/auth', authRouter);
-server.use('/api/reviews', reviewRouter);
+
 
 server.get('/', (req, res) => {
     res.send('<h1>Backend API for FoodieFun</h1>');
